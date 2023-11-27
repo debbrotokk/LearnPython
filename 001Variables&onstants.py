e4e0a82b7a7445355653710b8366bf205fdc7ea8
debbrotokumark <debkk20@gmail.com>
2023-11-27 14:46:11 +0600

    first commit

---
+++ 001Variables&onstants.py
@@ -0,0 +1,17 @@
+print("Write something here")
+name ="debbroto"
+print(name)
+midtitle,lasttitle="KUMAR","Karmokar"
+print(midtitle,lasttitle)
+# print(midtitle)
+# print(lasttitle)
+village='bichat'
+print(village)
+
+a=10
+b=9,7
+c=3739382.938
+print(a,b,c)
+a=b=c=10
+print(a,b,c)
+
